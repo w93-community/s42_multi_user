db98885b6e3f454f3275aee220672694947f8296
DomenicWaterdash <ms.kreis8@t-online.de>
2018-06-06 22:58:17 +0200

    big update to multi-user

--- multi-user.js
+++ multi-user.js
@@ -1,9 +1,17 @@
 //multi-user concept
 
 //insert code that creates user directory with default user
-le._path.home = "/a/users/default/"
-le._path.desktop = "/a/users/default/desktop"
-//  le._path.skin = "/a/users/default/skin
+function loginUser(username){
+le._path.home = "/a/users/" + username + "/"
+le._path.desktop = "/a/users/" + username + "/desktop/"
+//  le._path.skin = "/a/users/" + username + "/skin/"
 // uncomment above line once/if custom skins for users were implemented
-
 // IMPORTANT: insert code for settings.json, since i cant figure that out
+  localforage.getItem('/a/users/' + username + '/boot/boot.js').then(function(okthen){
+    eval(okthen) //ok yes this is unsafe, but i will change it later
+    //ok so this boot idea doesnt work yet
+  })
+}
+
+//DEMO CODE
+loginUser("default") // /a/users/default/ should exist, WITH all other directories mentioned in the function
