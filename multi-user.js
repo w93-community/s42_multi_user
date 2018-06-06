194c578eda12e58fb5d785b05da9c34900996691
DomenicWaterdash <ms.kreis8@t-online.de>
2018-06-06 22:32:46 +0200

    multi-user concept

---
+++ multi-user.js
@@ -0,0 +1,9 @@
+//multi-user concept
+
+//insert code that creates user directory with default user
+le._path.home = "/a/users/default/"
+le._path.desktop = "/a/users/default/desktop"
+//  le._path.skin = "/a/users/default/skin
+// uncomment above line once/if custom skins for users were implemented
+
+// IMPORTANT: insert code for settings.json, since i cant figure that out
