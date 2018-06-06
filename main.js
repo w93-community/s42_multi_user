b9f194323688d74b634440e8d4bf410b21ab05ec
DomenicWaterdash <ms.kreis8@t-online.de>
2018-06-06 22:14:53 +0200

    fix var name in main

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // to be ran with /a/boot
 let params = (new URL(document.location)).searchParams;
 let pass = params.get("pass"); //windows93.net?pass="value"
-var sys-pass = "demo" //testing purposes
+var sys_pass = "demo" //testing purposes
 if pass = !sys-pass {
 bootSetup();
 $boot.BIOS.innerHTML = "Incorrect Password " + pass +" !"
