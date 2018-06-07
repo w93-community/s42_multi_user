d225aa79b5470b78be59c4c03a83db38cc52580c
DomenicWaterdash <ms.kreis8@t-online.de>
2018-06-07 16:41:39 +0200

    add createUser into multi-user.js

--- multi-user.js
+++ multi-user.js
@@ -1,15 +1,13 @@
 //multi-user concept
-
-//insert code that creates user directory with default user
 function loginUser(username){
 le._path.home = "/a/users/" + username + "/"
 le._path.desktop = "/a/users/" + username + "/desktop/"
-//  le._path.skin = "/a/users/" + username + "/skin/"
-// uncomment above line once/if custom skins for users were implemented
-// IMPORTANT: insert code for settings.json, since i cant figure that out
-  console.log($store.getRaw('users/default/boot/boot.js'))
-eval($store.getRaw('users/default/boot/boot.js')) // boot code attempt, probably doesnt work
+// le._settings = JSON.parse($store.getRaw("/a/users/" + username + "/settings.json"))
 }
-
+function createUser(username){
+  localforage.setItem('users/default/settings.json', 'hello world')
+  localforage.setItem('users/default/desktop/welcome.txt', 'This is the desktop for your user, whatever you put into here will only be for this user')
+}
 //DEMO CODE
-loginUser("default") // /a/users/default/ should exist, WITH all other directories mentioned in the function
+createUser("default") // create default user
+loginUser("default") // login as user
