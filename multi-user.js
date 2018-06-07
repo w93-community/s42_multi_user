6cbaaa8dbbd7c27da20c9c21e7d148fc6f6c6e7b
DracoZ12 <DracoZ12@users.noreply.github.com>
2018-06-07 17:14:27 +0200

    now it works!

--- multi-user.js
+++ multi-user.js
@@ -1,13 +1,16 @@
-//multi-user concept
-function loginUser(username){
-le._path.home = "/a/users/" + username + "/"
-le._path.desktop = "/a/users/" + username + "/desktop/"
-// le._settings = JSON.parse($store.get("users/" + username + "/settings.json"))
+function loginUser (username) {
+    le._path.home = "/a/users/" + username + "/";
+    le._path.desktop = "/a/users/" + username + "/desktop/";
+    le._settings = $store.get("users/" + username + "/settings.json");
 }
-function createUser(username){
-  localforage.setItem('users/default/settings.json', 'hello world')
-  localforage.setItem('users/default/desktop/welcome.txt', 'This is the desktop for your user, whatever you put into here will only be for this user')
+function createUser (username) {
+  localStorage.setItem("users/"+username+"/settings.json", localStorage["settings.json"]);
+  localStorage.setItem("users/"+username+"/desktop/welcome.txt", "This is the desktop for your user, whatever you put into here will only be for this user");
 }
-//DEMO CODE
-createUser("default") // create default user
-loginUser("default") // login as user
+/* 
+  How to use this:
+    createUser("hello");
+        ==> Creates an user named "hello".
+    loginUser("hello");
+        ==> Logs the user "hello" in.
+*/
